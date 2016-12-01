38205193dbc8918c484828409397976a23541fc6
kgit1 <kgit1@rambler.ru>
2016-12-01 22:03:15 +0200

    1

--- src/music1/ExTestDrive.java
+++ src/music1/ExTestDrive.java
@@ -1,36 +1,36 @@
 package music1;
 
-class MyEx extends Exception{}
+class MyEx extends Exception {
+}
 
 public class ExTestDrive {
 
 	public static void main(String[] args) {
-		//fake cmd lin argument
+		// fake cmd lin argument
 		args = new String[1];
 		args[0] = "no";
-		
+
 		String test = args[0];
-		
-		try{
+
+		try {
 			System.out.println("t");
-			
+
 			doRisry(test);
 			System.out.println("o");
-		}catch(MyEx e){
+		} catch (MyEx e) {
 			System.out.println("a");
-			
-		}finally{
+
+		} finally {
 			System.out.println("w");
 		}
 		System.out.println("s");
 	}
 
-	static void doRisry(String t)throws MyEx{
+	static void doRisry(String t) throws MyEx {
 		System.out.println("h");
-		if("yes".equals(t)){
+		if ("yes".equals(t)) {
 			throw new MyEx();
 		}
 		System.out.println("r");
 	}
 }
-
