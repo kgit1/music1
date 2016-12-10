dbba51c9b0cb056a78ed1c21ed00e1e3dfdbe8f1
kgit1 <kgit1@rambler.ru>
2016-12-10 19:58:55 +0200

    threads

---
+++ src/aThreads/RunThreads.java
@@ -0,0 +1,38 @@
+package aThreads;
+
+//thread to start needs runnable object with method run which tell to tread 
+//what to do
+//so we implements Runnable interface to class which will tell thread what to do
+public class RunThreads implements Runnable {
+
+	public static void main(String[] args) {
+		// instantiate
+		RunThreads runner = new RunThreads();
+		// create thread and give him Runnable object with directives
+		// but thread will be just created but not started
+		Thread alpha = new Thread(runner);
+		// create thread and give him Runnable object with directives
+		// but thread will be just created but not started
+		Thread beta = new Thread(runner);
+		// name the thread(optional)
+		alpha.setName("Thread Alpha");
+		// name the thread(optional)
+		beta.setName("Thread Beta");
+		// start the thread
+		alpha.start();
+		// start the thread
+		beta.start();
+		for (int i = 0; i < 25; i++) {
+			System.out.println("-----Thread MAIN----");
+		}
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 25; i++) {
+			String threadName = Thread.currentThread().getName();
+			System.out.println(threadName);
+		}
+	}
+
+}
