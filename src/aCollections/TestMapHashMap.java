131d18bd82f25a81561329c46bea2dfdab71c1b4
kgit1 <kgit1@rambler.ru>
2016-12-14 19:38:01 +0200

    up

--- src/aCollections/TestMapHashMap.java
+++ src/aCollections/TestMapHashMap.java
@@ -1,6 +1,8 @@
 package aCollections;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class TestMapHashMap {
 
@@ -15,6 +17,24 @@ public class TestMapHashMap {
 		System.out.println(scores);
 		System.out.println(scores.get("Bert"));
 
+//		ArrayList<Dog> dogs1 = new ArrayList<Animal>();
+//		ArrayList<Animal> animals = new ArrayList<Dog>();
+		List<Animal> list = new ArrayList<Animal>();
+		ArrayList<Dog> dogs = new ArrayList<Dog>();
+		List<Dog> dogs2 = new ArrayList<Dog>();
+//		ArrayList<Animal> a = dogs;
+		ArrayList<Dog> d = dogs;
+		List<Dog> ld = dogs;
+		list(d);
+		list(dogs);
+//		list(dogs2);
+	}
+	
+	static public void list(ArrayList<Dog> d){}
+
+	public class Animal {
 	}
 
+	public class Dog extends Animal {
+	}
 }
