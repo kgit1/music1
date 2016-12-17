2734d2470e2aa5759958bb03265f475ea3f5a220
kgit1 <kgit1@rambler.ru>
2016-12-18 01:14:14 +0200

    Selection Sort and Insertion Sort

--- src/aFib/Sort.java
+++ src/aFib/Sort.java
@@ -1,5 +1,12 @@
 package aFib;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -7,7 +14,15 @@ public class Sort {
 
 	public static void main(String[] args) {
 		Sort s = new Sort();
-		s.printAndSort(s.arrayGenerator(150, 50));
+		s.writeArray(s.arrayGenerator(150, 50));
+		int[] arr = s.readArray();
+		int[] arr1 = new int[50];
+		arr1 = s.readArray();
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr1));
+		s.printAndSort(arr);
+		s.printAndSort1(arr1);
+		// s.sortInsertion(s.arrayGenerator(50, 50));
 	}
 
 	// generate array with random numbers up to "randMax" with size - "size"
@@ -20,8 +35,7 @@ public class Sort {
 		return array;
 	}
 
-	// sort array with selection algorithm
-	// 
+	// sort array with Selection Sort
 	public void sortSelection(int[] array) {
 		int counter = 0;
 		for (int i = 0; i < array.length - 1; i++) {
@@ -42,7 +56,20 @@ public class Sort {
 			}
 		}
 		System.out.println("Counter: " + counter);
+	}
 
+	// sort array with Insertion Sort
+	public void sortInsertion(int[] array) {
+		int counter = 0;
+		for (int i = 1; i < array.length; i++) {
+			for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
+				counter++;
+				int tmp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = tmp;
+			}
+		}
+		System.out.println(counter);
 	}
 
 	// print incoming array and sorted version
@@ -50,6 +77,47 @@ public class Sort {
 		System.out.println("Random array       " + Arrays.toString(array));
 		sortSelection(array);
 		System.out.println("Random sorted array" + Arrays.toString(array));
+	}
+
+	// print incoming array and sorted version
+	public void printAndSort1(int[] array) {
+		System.out.println("Random array       " + Arrays.toString(array));
+		sortInsertion(array);
+		System.out.println("Random sorted array" + Arrays.toString(array));
+	}
+
+	// write array to file
+	public void writeArray(int[] array) {
+		File file = new File("TestArray.txt");
+		try {
+			FileOutputStream out = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(out);
+			oos.writeObject(array);
+			oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// get array from file
+	public int[] readArray() {
+		int[] array = new int[50];
+		File file = new File("TestArray.txt");
+		try {
+			FileInputStream in = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(in);
+			array = (int[]) ois.readObject();
+			ois.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return array;
 	}
 
 }
