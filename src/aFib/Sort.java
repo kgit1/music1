6460bf0fe2ed992ac4c01b43d0c15e172bf24493
kgit1 <kgit1@rambler.ru>
2016-12-17 20:41:33 +0200

    sort

--- src/aFib/Sort.java
+++ src/aFib/Sort.java
@@ -3,10 +3,11 @@ package aFib;
 import java.util.Arrays;
 import java.util.Random;
 
-public class sortChoose {
+public class Sort {
 
 	public static void main(String[] args) {
-		arrayGenerator(2, 50);
+		Sort s = new Sort();
+		s.printAndSort(s.arrayGenerator(150, 50));
 	}
 
 	public static void sortChoose(int[] arr) {
@@ -25,20 +26,19 @@ public class sortChoose {
 		}
 	}
 
-	public static void randArray(int size) {
-		int[] arr = new int[size];
-		Random rand = new Random();
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = rand.nextInt(150) + 1;
-		}
-		System.out.println("Random array       " + Arrays.toString(arr));
-		sortChoose(arr);
-		System.out.println("Random sorted array" + Arrays.toString(arr));
+	public void printAndSort(int[] array) {
+		System.out.println("Random array       " + Arrays.toString(array));
+		sortChoose(array);
+		System.out.println("Random sorted array" + Arrays.toString(array));
 	}
 
-	public static void arrayGenerator(int number, int size) {
-		for (int i = 0; i < number; i++) {
-			randArray(size);
+	public int[] arrayGenerator(int randMax, int size) {
+		int[] array = new int[size];
+		Random rand = new Random();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(randMax);
 		}
+		return array;
 	}
+
 }
