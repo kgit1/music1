1dcd31f40b822acf19658e70a92bd547a449a2e1
kgit1 <kgit1@rambler.ru>
2016-12-25 01:36:56 +0200

    up

--- src/project/Project.java
+++ src/project/Project.java
@@ -507,22 +507,57 @@ public class Project {
 // }
 // }
 
-
-///**
+/// **
 // * Shellsort, using a sequence suggested by Gonnet.
 // * @param a an array of Comparable items.
 // */
-//public static void shellsort( Comparable [ ] a )
-//{
-//    for( int gap = a.length / 2; gap > 0;
-//                 gap = gap == 2 ? 1 : (int) ( gap / 2.2 ) )
-//        for( int i = gap; i < a.length; i++ )
-//        {
-//            Comparable tmp = a[ i ];
-//            int j = i;
+// public static void shellsort( Comparable [ ] a )
+// {
+// for( int gap = a.length / 2; gap > 0;
+// gap = gap == 2 ? 1 : (int) ( gap / 2.2 ) )
+// for( int i = gap; i < a.length; i++ )
+// {
+// Comparable tmp = a[ i ];
+// int j = i;
 //
-//            for( ; j >= gap && tmp.compareTo( a[ j - gap ] ) < 0; j -= gap )
-//                a[ j ] = a[ j - gap ];
-//            a[ j ] = tmp;
-//        }
-//}
+// for( ; j >= gap && tmp.compareTo( a[ j - gap ] ) < 0; j -= gap )
+// a[ j ] = a[ j - gap ];
+// a[ j ] = tmp;
+// }
+// }
+
+//comb sort
+
+// The basic idea is to eliminate turtles, or small values near the end of the
+// list, since in a bubble
+// sort these slow the sorting down tremendously.Rabbits, large values around
+// the
+// beginning of the list,do not pose a problem in bubble sort. In bubble sort,
+// when any two elements are compared, they always have
+//
+// a gap (distance from each other) of 1. The basic idea of comb sort is that
+// the gap can be much more than 1. The inner loop of bubble sort, which does
+// the actual swap, is modified such that gap between swapped elements goes down
+// (for each iteration of outer loop) in steps of a "shrink factor" k: [ n/k,
+// n/k2, n/k3, ..., 1 ].
+//
+// The gap starts out as the length of the list n being sorted divided by the
+// shrink factor k (generally 1.3; see below) and one pass of the aforementioned
+// modified bubble sort is applied with that gap. Then the gap is divided by the
+// shrink factor again, the list is sorted with this new gap, and the process
+// repeats until the gap is 1. At this point, comb sort continues using a gap of
+// 1 until the list is fully sorted. The final stage of the sort is thus
+// equivalent to a bubble sort, but by this time most turtles have been dealt
+// with, so a bubble sort will be efficient.
+//
+// The shrink factor has a great effect on the efficiency of comb sort. k = 1.3
+// has been suggested as an ideal shrink factor by the authors of the original
+// article after empirical testing on over 200,000 random lists. A value too
+// small slows the algorithm down by making unnecessarily many comparisons,
+// whereas a value too large fails to effectively deal with turtles.
+//
+// The pattern of repeated sorting passes with decreasing gaps is similar to
+// Shellsort, but in Shellsort the array is sorted completely each pass before
+// going on to the next-smallest gap. Comb sort's passes do not completely sort
+// the elements. This is the reason that Shellsort gap sequences have a larger
+// optimal shrink factor of about 2.2.
