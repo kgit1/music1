96dd52e86cb3814dbbc4f1766f63dce7f7858793
kgit1 <kgit1@rambler.ru>
2016-12-29 22:40:55 +0200

    up

--- src/aLoggingException/DynamicFields.java
+++ src/aLoggingException/DynamicFields.java
@@ -48,6 +48,43 @@ public class DynamicFields {
 		}
 		return -1;
 	}
-	
-	
+
+	// method returns index of the key(the first element of pair) if such exists
+	// in our list, else throws NoSuchFieldException
+	private int getFieldNumber(String id) throws NoSuchFieldException {
+		int fieldNum = hasField(id);
+		if (fieldNum == -1) {
+			throw new NoSuchFieldException();
+		}
+		return fieldNum;
+	}
+
+	// method makes new key in our list
+	private int makeField(String id) {
+		// iterate through our list trying to find empty field
+		// if empty found - assign id to it, and return it's index
+		for (int i = 0; i < fields.length; i++) {
+			if (fields[i][0] == null) {
+				fields[i][0] = id;
+				return i;
+			}
+		}
+		// else create new multidimensional array of Objects with
+		// fields.length+1
+		// containing arrays of pairs of objects
+		Object[][] tmp = new Object[fields.length + 1][2];
+
+		// copy our list fields to new array
+		for (int i = 0; i < fields.length; i++) {
+			tmp[i] = fields[i];
+		}
+		// initialize last(new) element in new array with objects array consists
+		// of pair of elements
+		for (int i = fields.length; i < tmp.length; i++) {
+			tmp[i] = new Object[][] { null, null };
+		}
+		// recursive call our method second time to finish adding item to list
+		return makeField(id);
+	}
+
 }
