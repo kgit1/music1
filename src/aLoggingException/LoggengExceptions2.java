4cbd96495b85247362859eed654794b082de43aa
kgit1 <kgit1@rambler.ru>
2016-12-29 21:04:13 +0200

    up

--- src/aLoggingException/LoggengExceptions2.java
+++ src/aLoggingException/LoggengExceptions2.java
@@ -6,23 +6,28 @@ import java.util.logging.Logger;
 
 public class LoggengExceptions2 {
 
-	//create logger object with String with name of the Object
-	//this object returns his out put in System.err form
+	// create logger object with String with name of the Object
+	// this object returns his out put in System.err form
 	private static Logger logger = Logger.getLogger("Logging exception");
 
 	static void logException(Exception e) {
 		StringWriter trace = new StringWriter();
-		//create e.printStackTrace(), usually it dosn't return String, 
-		//so we need printStackTrace with PrintWriter in argument
+		// create e.printStackTrace(), usually it dosn't return String,
+		// so we need printStackTrace with PrintWriter in argument
 		e.printStackTrace(new PrintWriter(trace));
 		logger.severe(trace.toString());
 	}
 
 	public static void main(String[] args) {
-		try{
-			throw new NullPointerException();
-		}catch(NullPointerException e){
+		try {
+			int a = 0;
+			int b = 2;
+			int c = b / a;
+			 System.out.println(c);
+		} catch (Exception e) {
 			logException(e);
+			System.out.println("===========");
+			e.printStackTrace();
 		}
 	}
 
