ab4f458e48e277c430b3dd9154d461c6c38b71c4
kgit1 <kgit1@rambler.ru>
2017-01-03 19:01:34 +0200

    up

--- src/nullPerson/Staff.java
+++ src/nullPerson/Staff.java
@@ -14,9 +14,75 @@ public class Staff extends ArrayList<Position> {
 		}
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Staff(String... titles) {
+		add(titles);
+	}
 
+	public boolean positionAvaileble(String title) {
+		for (Position position : this) {
+			if (position.getTitle().equals(title)
+					&& position.getPerson() == Person.NULL) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void fillPosition(String title, Person hire) {
+		for (Position position : this) {
+//			System.out.println(position+"=============");
+			if (position.getTitle().equals(title)
+					&& position.getPerson() == Person.NULL) {
+				position.setPerson(hire);
+				return;
+			}
+		}
+		throw new RuntimeException(
+				"Position " + title + " is not available");
+	}
+
+	public static void main(String[] args) {
+		Staff staff = new Staff("President", "CTO", "Marketing manager",
+				"Product Manager", "Project Lead", "Software Engeneer",
+				"Software Engeneer", "Software Engeneer", "Software Engeneer",
+				"Test Engeneer");
+//		printStaff(staff);
+//		System.out.println("====================");
+//		sleeep();
+		staff.fillPosition("President",
+				new Person("Me", "Last", "The Top, Lonely Art"));
+//		printStaff(staff);
+//		sleeep();
+		staff.fillPosition("CTO", new Person("Janet", "Planner", "the Burbs"));
+//		printStaff(staff);
+//		sleeep();
+		if (staff.positionAvaileble("Software Engeneer")) {
+			staff.fillPosition("Software Engeneer",
+					new Person("Bob", "Coder", " Bright Ligth City"));
+		}
+		if (staff.positionAvaileble("Software Engeneer")) {
+			staff.fillPosition("Software Engeneer",
+					new Person("Bob1", "Coder", " Bright Ligth City"));
+		}
+		if (staff.positionAvaileble("Software Engeneer")) {
+			staff.fillPosition("Software Engeneer",
+					new Person("Bob2", "Coder", " Bright Ligth City"));
+		}
+		printStaff(staff);
+	}
+
+	public static void printStaff(Staff staff) {
+		for (Position position : staff) {
+			System.out.println(position);
+		}
+	}
+
+	public static void sleeep() {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
