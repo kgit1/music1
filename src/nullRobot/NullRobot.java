df62c114f7a39971bb93cb69262a2d001ddae160
kgit1 <kgit1@rambler.ru>
2017-01-03 19:40:26 +0200

    up

---
+++ src/nullRobot/NullRobot.java
@@ -0,0 +1,23 @@
+package nullRobot;
+
+import java.lang.reflect.Proxy;
+
+public class NullRobot {
+
+	public static Robot newNullRobot(Class<? extends Robot> type) {
+		return (Robot) Proxy.newProxyInstance(NullRobot.class.getClassLoader(),
+				new Class[] { Null.class, Robot.class },
+				new NullRobotProxyHandler(type));
+	}
+
+	public static void main(String[] args) {
+		Robot[] bots = {
+				new SnowRemovalRobot("SnowBee"),
+				newNullRobot(SnowRemovalRobot.class)
+		};
+		for (Robot robot : bots) {
+			Robot.Test.test(robot);
+		}
+	}
+
+}
--- src/nullRobot/SnowRemovalRobot.java
+++ src/nullRobot/SnowRemovalRobot.java
@@ -3,6 +3,8 @@ package nullRobot;
 import java.util.Arrays;
 import java.util.List;
 
+import sun.applet.Main;
+
 public class SnowRemovalRobot implements Robot {
 
 	String name;
@@ -25,13 +27,33 @@ public class SnowRemovalRobot implements Robot {
 	public List<Operation> operations() {
 		return Arrays.asList(new Operation() {
 			public String description() {
-				return name + " removes snow";
+				return name + " can remove snow";
 			}
 
 			public void command() {
 				System.out.println(name + " removes snow");
 			}
+		}, new Operation() {
+			public String description() {
+				return name + " can brake ice";
+			}
+
+			public void command() {
+				System.out.println(name + " brakes ice");
+			}
+		}, new Operation() {
+			public String description() {
+				return name + " can clean uu the roof";
+			}
+
+			public void command() {
+				System.out.println(name + " cleans up the roof");
+			}
 		});
+	}
+	
+	public static void main(String[] args) {
+		Robot.Test.test(new SnowRemovalRobot("Slusher"));
 	}
 
 }
