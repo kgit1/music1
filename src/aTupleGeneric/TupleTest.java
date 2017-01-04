947e2b5ec9268dd5ae876502605f9db23168c5c5
kgit1 <kgit1@rambler.ru>
2017-01-04 23:06:39 +0200

    up

--- src/aTupleGeneric/TupleTest.java
+++ src/aTupleGeneric/TupleTest.java
@@ -1,16 +1,41 @@
 package aTupleGeneric;
 
 //tuple - "kortez" - method how to return couple of elements not just one
+//too return more than one parameter via return in method
+//we create class with so many parameters how many we need to return 
+//and with types which they must be
+//and then just return this parameters through object of this class
 public class TupleTest {
+	static TwoTuple<String, Integer> f() {
+		return new TwoTuple("hi", 47);
+	}
+
+	static ThreeTuple<Amphibian, String, Integer> g() {
+		return new ThreeTuple(new Amphibian(), "hi", 47);
+	}
+
+	static FourTuple<Vehicle, Amphibian, String, Integer> h() {
+		return new FourTuple(new Vehicle(), new Amphibian(), "hi", 47);
+	}
+
+	static FiveTuple<Vehicle, Amphibian, String, Integer, Double> k() {
+		return new FiveTuple(new Vehicle(), new Amphibian(), "hi", 47, 11.1);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TwoTuple<String, Integer> ttsi = f();
+		System.out.println(ttsi);
 
+		System.out.println(g());
+		System.out.println(h());
+		System.out.println(k());
 	}
 
 }
 
 class TwoTuple<A, B> {
+
+	// final make field unchangeable after construction
 	public final A first;
 	public final B second;
 
@@ -71,4 +96,10 @@ class FiveTuple<A, B, C, D, E> extends FourTuple<A, B, C, D> {
 				+ ", " + fifth + ")";
 	}
 
+}
+
+class Amphibian {
+}
+
+class Vehicle {
 }
