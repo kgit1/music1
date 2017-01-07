34b589468dec8b8d8d7d5dda15e225ad38b92baa
kgit1 <kgit1@rambler.ru>
2017-01-07 18:48:42 +0200

    up

--- src/quizes/BankTeller.java
+++ src/quizes/BankTeller.java
@@ -9,6 +9,8 @@ import java.util.Random;
 
 import aFib.Generator;
 
+//demonstrates two classes haven't implementation of one interface, 
+//but still working together and using one interface with help of inside anonymous classes of one interface
 public class BankTeller {
 
 	public static void serve(Teller t, Customer c) {
@@ -31,17 +33,24 @@ public class BankTeller {
 
 class Customer {
 
+	// create static variable for counter
 	private static long counter = 1;
+	// create unchangeable private variable to hold counter and increment it
+	// every time object created
 	private final long id = counter++;
 
-	public Customer() {
+	// empty private constructor
+	private Customer() {
 	}
 
 	@Override
+	// toString returning objects id to keep tracking
 	public String toString() {
 		return "Customer [id=" + id + "]";
 	}
 
+	// method using anonymous class of interface Generator
+	// to return new object of this class through method next()
 	public static Generator<Customer> generator() {
 		return new Generator<Customer>() {
 			public Customer next() {
@@ -53,18 +62,26 @@ class Customer {
 
 class Teller {
 
+	// create static variable for counter
 	private static long counter = 1;
+	// create unchangeable private variable to hold counter and increment it
+	// every time object created
 	private final long id = counter++;
 
-	public Teller() {
+	// empty private constructor
+	private Teller() {
 	}
 
 	@Override
+	// toString returning objects id to keep tracking
 	public String toString() {
 		return "Teller [id=" + id + "]";
 	}
 
+	// anonymous class of interface Generator
+	// with method next() creating new object of this class
 	public static Generator<Teller> generator = new Generator<Teller>() {
+		// method inside anonymous class
 		public Teller next() {
 			return new Teller();
 		};
